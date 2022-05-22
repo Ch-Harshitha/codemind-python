9224432610ec16300f8a9cce6e322fc60b761eb5
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-05-22 22:33:07 +0530

    def count(n):     c=0     while(n!=0):         c+=1         n=n//10     return c def first(n,x):     len=count(n)     while(len>x):         n=n//10         len-=1     first=n     return first def last(n,x):     i=0     m=1     while(i<x):         m=m*10         i+=1     last=n%m     return last N,X=map(int,input().split()) print(abs(first(N,X)-last(N,X)))

--- Absolute_difference_numbers.py
+++ Absolute_difference_numbers.py
@@ -2,22 +2,22 @@ def count(n):
     c=0
     while(n!=0):
         c+=1
-        n//=10
+        n=n//10
     return c
-def last(n,x):
-    i=0
-    mod=1
-    while(i<x):
-        mod=mod*10
-        i+=1
-    last=n%mod
-    return last
 def first(n,x):
-    length=count(N)
-    while(length!=x):
-        n//=10
-        length-=1
+    len=count(n)
+    while(len>x):
+        n=n//10
+        len-=1
     first=n
     return first
+def last(n,x):
+    i=0
+    m=1
+    while(i<x):
+        m=m*10
+        i+=1
+    last=n%m
+    return last
 N,X=map(int,input().split())
 print(abs(first(N,X)-last(N,X)))
