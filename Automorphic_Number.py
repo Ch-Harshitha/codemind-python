ab236ea7bbd589ceae9ba5e594f0610fc274e00f
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-05-31 21:43:04 +0530

    def count(n):     c=0     while(n!=0):         c+=1         n//=10     return c n=int(input()) s=n*n p=s%(10**count(n)) if(p==n):     print('Automorphic Number'); else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,7 +1,13 @@
+def count(n):
+    c=0
+    while(n!=0):
+        c+=1
+        n//=10
+    return c
 n=int(input())
 s=n*n
-l=10**len(str(n))
-if(s%l==n):
-    print('Automorphic Number')
+p=s%(10**count(n))
+if(p==n):
+    print('Automorphic Number');
 else:
     print('Not an Automorphic Number')
