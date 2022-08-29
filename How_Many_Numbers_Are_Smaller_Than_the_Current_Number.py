bcca07fa83bd5ca36219268550e33a07e25fabf3
Ch-Harshitha <96976684+Ch-Harshitha@users.noreply.github.com>
2022-08-29 21:56:06 +0530

    n=int(input()) a=list(map(int,input().split())) for i in range(n):     c=0     for j in range(n):         if(i!=j):             if(a[i]>a[j]):                 c+=1     print(c,end=' ')

--- How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,9 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
-for i in range (n):
+for i in range(n):
     c=0
-    for j in range (n):
-        if i!=j:
-            if a[i]>a[j]:
+    for j in range(n):
+        if(i!=j):
+            if(a[i]>a[j]):
                 c+=1
     print(c,end=' ')
